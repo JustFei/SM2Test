1493593fe8db817715e1a6159f2d9c0b5076cbbe
莫福见 <mofujian@danwenhaodemacbook-air.local>
2016-07-25 10:21:12 +0800

    Initial Commit

---
+++ SM2测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SM2测试
+//
+//  Created by 莫福见 on 16/7/25.
+//  Copyright © 2016年 Manridy.Bobo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SM2测试Tests/SM2__Tests.m
@@ -0,0 +1,39 @@
+//
+//  SM2__Tests.m
+//  SM2测试Tests
+//
+//  Created by 莫福见 on 16/7/25.
+//  Copyright © 2016年 Manridy.Bobo.com. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SM2__Tests : XCTestCase
+
+@end
+
+@implementation SM2__Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
